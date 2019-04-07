ca73053a124abbea3760e313247be12bfcc3d538
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 23:53:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array["element1", "element2", ]
+  array["elem", "eleme", "elemen"]
+  array
 end
 
 def add_element_to_start_of_array(array, element)
