f5e66c453e48662d7f2793f0f6fb2a823ce89d3e
Stella McEwen <mcewen.stella@gmail.com>
2019-04-08 00:01:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["beginning", "middle", "end", "arrays!"]
-  array.pop
+  answer = array.pop
 end
 
 def remove_element_from_start_of_array(array)
